3ba6c78cd1f6d4ba2a5e506eace37861ad482560
jvalrog <javiervalencia80@gmail.com>
2019-04-29 14:18:30 +0100

    Simplificada la instalacion

--- ansible/instalar-ansible.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo add-apt-repository -uy ppa:ansible/ansible
-sudo apt-get install -y ansible
--- ansible/instalar-maestro.sh
+++ ansible/instalar-maestro.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-ansible-playbook master.yml -K -i hosts_local
+# INSTALAR ANSIBLE
+
+sudo add-apt-repository -uy ppa:ansible/ansible
+sudo apt-get install -y ansible
+
+# INSTALAR ICINGA2
+
+ansible-playbook master.yml -K -i hosts
